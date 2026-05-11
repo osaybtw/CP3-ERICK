ce0acd00358f79570c787e27a8cb5a08951648ec
gameloque <gabrielhenriqueff1448@gmail.com>
2026-05-11 10:58:56 -0300

    Painel Inicial do Caixa

--- CP3.c
+++ CP3.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
+float saldo;
+int usuario;
+float saldo = 0;
 int main(){
+    printf("======================================================\n");
+    printf("================  CAIXA MASSA DA FIAP ================\n");
+    printf("======================================================\n");
+    printf("======  Seu saldo e de: R$%.2f =======================\n", saldo);
+    printf("======================================================\n");
+
+
 }
