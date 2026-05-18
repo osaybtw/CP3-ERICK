3429e52c2a0745a075595b4982124ce673ec3949
Caioo <caiom9249@gmail.com>
2026-05-18 00:34:37 -0300

    Readiciona arquivo CP3.c ao projeto

---
+++ cp3.c/cp3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+float saldo;
+int usuario;
+int usuario, opcao;
+float saldo = 0;
+int main(){
+    printf("======================================================\n");
+    printf("================  CAIXA MASSA DA FIAP ================\n");
+    printf("======================================================\n");
+    printf("======  Seu saldo e de: R$%.2f =======================\n", saldo);
+    printf("======================================================\n");
+
+
+}
+int main(){
+  
+    printf("Saldo inicial: R$ %.2f\n\n", saldo);
+    
+  
+    do {
+        system("cls || clear");  
+        exibirMenu();
+        
+        
+        scanf("%d", &opcao);
+        limparBuffer();  
+        
+        
+        
+        
+        
+        if(opcao != 0) {
+            printf("\nPressione Enter para continuar...");
+            getchar();
+        }
+        
+    } while(opcao != 0);
+    
+    return 0;
+
+}
